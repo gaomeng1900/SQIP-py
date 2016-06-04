fa2fa08c663e9185ed04f2d2899039110bf8e29a
Meng G <gaomeng1900@gmail.com>
2016-06-04 20:24:00 +0800

    删除冗余代码

--- sqip/api/models/__init__.py
+++ sqip/api/models/__init__.py
@@ -5,11 +5,11 @@
 from sqip.config import *
 from sqip.libs import *
 
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+# from flask import Flask
+# from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app)
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# db = SQLAlchemy(app)
 
